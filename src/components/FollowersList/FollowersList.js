6f2506663ef39c559f9f40dd0f90ff84e9984a99
Tigran-105 <tik.sargsyan105@gmail.com>
2022-04-20 16:49:55 +0400

    testing end

--- src/components/FollowersList/FollowersList.js
+++ src/components/FollowersList/FollowersList.js
@@ -8,20 +8,23 @@ export default function FollowersList() {
     const [followers, setFollowers] = useState([]);
 
     useEffect(() => {
+
+        const fetchFollowers = async () => {
+            const { data } = await axios.get("https://randomuser.me/api/?results=5")
+            setFollowers(data.results)
+        }
+
         fetchFollowers()
     }, []);
 
-    const fetchFollowers = async () => {
-        const {data} = await axios.get("https://randomuser.me/api/?results=15")
-        setFollowers(data.results)
-    }
+
 
     return (
         <div className="followerslist-container">
             <div>
                 {followers.map((follower, index) => (
-                    <div key={follower.id} className="follower-item">
-                        <img src={follower.picture.large} alt={`follower ${index}`}/>
+                    <div key={index} className="follower-item" data-testid={`follower-item-${index}`}>
+                        <img src={follower.picture.large} alt={`follower-${index}`}/>
                         <div className="followers-details">
                             <div className="follower-item-name">
                                 <h4>{follower.name.first}</h4> <h4>{follower.name.last}</h4>
